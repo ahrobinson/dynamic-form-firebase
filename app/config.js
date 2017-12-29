081f558acc48bec7490bccd837ae53e8def7f43d
ahrobinson <ahrobins3@gmail.com>
2017-12-29 10:42:16 -0600

    update git ignore

--- app/config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  user: 'alonrobinson', // name of the user account
-  database: 'alonrobinson', // name of the database
-  host: 'localhost',
-  port: 5432,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
-};
